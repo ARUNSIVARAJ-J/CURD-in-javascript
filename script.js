cd5bf7cb5dfe9f6273664b937b6f5f3a1fcfc0f0
ARUNSIVARAJ-J <114554392+ARUNSIVARAJ-J@users.noreply.github.com>
2022-12-26 22:04:20 +0530

    javascript curd file

---
+++ script.js
@@ -0,0 +1,84 @@
+
+var row=null;
+
+
+
+function Submit(){
+    var dataEntered=retrieveData();
+    var readData=readingDataFromLocalStorage(dataEntered);
+    if(dataEntered==false)
+    {
+        msg.innerHTML="please enter the data";
+    }else{
+    if(row == null){
+        insert(readData); 
+        msg.innerHTML="Data Inserted";
+    }else{
+        update();
+        msg.innerHTML="Data updatated"
+    }}
+    document.getElementById("form").reset();
+    
+}
+//CREATE
+//Reteriving data from form
+function retrieveData(){
+    var name1=document.getElementById("name").value;
+    var job=document.getElementById("job").value;
+    var exp=document.getElementById("exp").value;
+
+    var arr=[name1,job,exp]; 
+    if(arr.includes("")){
+        return false;
+    }else{
+        return arr;
+    }
+}
+//data in local storage
+function readingDataFromLocalStorage( dataEntered){
+    //store data in local storage
+    var n=localStorage.setItem("name",dataEntered[0]);
+    var j=localStorage.setItem("job",dataEntered[1]);
+    var e=localStorage.setItem("exp",dataEntered[2]);
+//getting value  local to table
+var n1= localStorage.getItem("name",n);
+var j1=localStorage.getItem("job",j);
+var e1=localStorage.getItem("exp",e);
+
+var arr=[n1,j1,e1];
+return arr;
+}
+//INSERT
+function insert(readData){
+    var row =table.insertRow();
+    row.insertCell(0).innerHTML=readData[0];
+    row.insertCell(1).innerHTML=readData[1];
+    row.insertCell(2).innerHTML=readData[2];
+    row.insertCell(3).innerHTML="<button onclick=edit(this)>Edit</button><button onclick=remove(this)>Delete</button>"
+
+    // cell1.innerHTML=readData[0];
+    // cell2.innerHTML=readData[1];
+    // cell3.innerHTML=readData[2];
+}
+//Edit
+function edit(td){
+   row=td.parentElement.parentElement;
+   document.getElementById("name").value=row.cells[0].innerHTML;
+   document.getElementById("job").value=row.cells[1].innerHTML;
+   document.getElementById("exp").value=row.cells[2].innerHTML;
+}
+//UPDATE
+function update(td){
+    row.cells[0].innerHTML=document.getElementById("name").value;
+    row.cells[1].innerHTML=document.getElementById("job").value;
+    row.cells[2].innerHTML=document.getElementById("exp").value;
+    row=null;  
+}
+//DELETE
+function remove(td){
+    var ans=confirm("Are you sure to delete the record");
+    if(ans==true){
+    row=td.parentElement.parentElement;
+    document.getElementById("table").deleteRow(row.rowIndex)
+    }  
+}
